c3f4caae928968f99904ff2a7af0491b13cf387d
Emeka Orji <emekapraiseo@gmail.com>
2024-06-24 21:51:23 +0100

    Refactor. Move account class into a separate file.

---
+++ src/types/index.ts
@@ -0,0 +1,44 @@
+export type Network = 'mainnet' | 'sepolia' | 'goerli';
+export type Tag = 'earliest' | 'pending' | 'latest';
+export type Sort = 'asc' | 'desc';
+export type BlockType = 'blocks' | 'uncles';
+
+export interface EtherScanConfig {
+  apikey: string;
+  network?: Network;
+}
+
+export interface BalanceResponse {
+  status: string;
+  message: string;
+  result: string;
+}
+
+export interface ABIResponse {
+  status: string;
+  message: string;
+  result: string;
+}
+
+export interface EtherscanParams {
+  address?: string;
+  tag?: Tag;
+  startBlock?: number;
+  endBlock?: number;
+  page?: number;
+  offset?: number;
+  sort?: Sort;
+  txHash?: string;
+  contractAddress?: string;
+  blockType?: BlockType;
+  blockNo?: number;
+}
+
+export interface GetBalanceOptions extends Pick<EtherscanParams, 'tag'> {}
+export interface BlockValidationOptions
+  extends Pick<EtherscanParams, 'page' | 'offset'> {}
+export interface TransactionOptions
+  extends Pick<
+    EtherscanParams,
+    'startBlock' | 'endBlock' | 'page' | 'offset' | 'sort'
+  > {}
