de8f76c137c93593123b39bba2352b4388c91fb2
Emeka Orji <emekapraiseo@gmail.com>
2024-06-24 21:39:19 +0100

    implement all endpoint in methods

--- src/client.ts
+++ src/client.ts
@@ -23,22 +23,24 @@ interface ABIResponse {
 interface EtherscanParams {
   address?: string;
   tag?: Tag;
-  startblock?: number;
-  endblock?: number;
+  startBlock?: number;
+  endBlock?: number;
   page?: number;
   offset?: number;
   sort?: Sort;
-  txhash?: string;
-  contractaddress?: string;
-  blocktype?: BlockType;
-  blockno?: number;
+  txHash?: string;
+  contractAddress?: string;
+  blockType?: BlockType;
+  blockNo?: number;
 }
 
 interface GetBalanceOptions extends Pick<EtherscanParams, 'tag'> {}
-interface GetTransactionsByAddressOptions
+interface BlockValidationOptions
+  extends Pick<EtherscanParams, 'page' | 'offset'> {}
+interface TransactionOptions
   extends Pick<
     EtherscanParams,
-    'startblock' | 'endblock' | 'page' | 'offset' | 'sort'
+    'startBlock' | 'endBlock' | 'page' | 'offset' | 'sort'
   > {}
 
 class Account {
@@ -84,16 +86,16 @@ class Account {
   /**
    * Get a list of 'Normal' Transactions By Address
    * @param {string} address the addresses to check for balance
-   * @param {number} options.startblock block number to start searching for transactions
-   * @param {number} options.endblock block number to stop searching for transactions
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
    * @param {number} options.page page number, if pagination is enabled
    * @param {number} options.offset the number of transactions displayed per page
-   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startblock and endblock range for faster search results.
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
    * @returns Returns the list of transactions performed by an address, with optional pagination.
    */
   public async getNormalTransactions(
     address: string,
-    { ...options }: GetTransactionsByAddressOptions = {}
+    { ...options }: TransactionOptions = {}
   ): Promise<BalanceResponse> {
     const url = this.etherScan.constructUrl('account', 'txlist', {
       address,
@@ -109,16 +111,16 @@ class Account {
   /**
    * Get a list of 'Internal' Transactions by Address
    * @param {string} address the addresses to check for balance
-   * @param {number} options.startblock block number to start searching for transactions
-   * @param {number} options.endblock block number to stop searching for transactions
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
    * @param {number} options.page page number, if pagination is enabled
    * @param {number} options.offset the number of transactions displayed per page
-   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startblock and endblock range for faster search results.
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
    * @returns Returns the list of transactions performed by an address, with optional pagination.
    */
   public async getInternalTransactions(
     address: string,
-    { ...options }: GetTransactionsByAddressOptions = {}
+    { ...options }: TransactionOptions = {}
   ): Promise<BalanceResponse> {
     const url = this.etherScan.constructUrl('account', 'txlistinternal', {
       address,
@@ -133,14 +135,14 @@ class Account {
 
   /**
    * Get 'Internal Transactions' by Transaction Hash
-   * @param {string} txhash the string representing the transaction hash to check for internal transactions
+   * @param {string} txHash the string representing the transaction hash to check for internal transactions
    * @returns Returns the list of internal transactions performed within a transaction.
    */
   public async getInternalTransactionsByHash(
-    txhash: string
+    txHash: string
   ): Promise<BalanceResponse> {
     const url = this.etherScan.constructUrl('account', 'txlistinternal', {
-      txhash,
+      txHash,
     });
     const response = await fetch(url);
     if (!response.ok) {
@@ -151,18 +153,173 @@ class Account {
 
   /**
    * Get "Internal Transactions" by Block Range
-   * @param {number} options.startblock block number to start searching for transactions
-   * @param {number} options.endblock block number to stop searching for transactions
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
    * @param {number} options.page page number, if pagination is enabled
    * @param {number} options.offset the number of transactions displayed per page
-   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startblock and endblock range for faster search results.
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
    * @returns Returns the list of internal transactions performed within a block range, with optional pagination.
    */
   public async getInternalTransactionsByBlockRange({
     ...options
-  }: GetTransactionsByAddressOptions = {}): Promise<BalanceResponse> {
+  }: TransactionOptions = {}): Promise<BalanceResponse> {
     const url = this.etherScan.constructUrl('account', 'txlistinternal', {
       ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get a list of 'ERC20 - Token Transfer Events' by Address
+   * @param {string} address the address to check for balance
+   * @param {string} contractAddress the token contract address to check for balance
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
+   * @param {number} options.page page number, if pagination is enabled
+   * @param {number} options.offset the number of transactions displayed per page
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
+   * @returns Returns the list of ERC-20 tokens transferred by an address, with optional filtering by token contract.
+   */
+  public async getERC20TokenEvents(
+    address: string,
+    contractAddress: string,
+    { ...options }: TransactionOptions = {}
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'tokentx', {
+      address,
+      contractAddress,
+      ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get a list of 'ERC721 - Token Transfer Events' by Address
+   * @param {string} address the address to check for balance
+   * @param {string} contractAddress the token contract address to check for balance
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
+   * @param {number} options.page page number, if pagination is enabled
+   * @param {number} options.offset the number of transactions displayed per page
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
+   * @returns Returns the list of ERC-721 ( NFT ) tokens transferred by an address, with optional filtering by token contract.
+   */
+  public async getERC721TokenEvents(
+    address: string,
+    contractAddress: string,
+    { ...options }: TransactionOptions = {}
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'tokennfttx', {
+      address,
+      contractAddress,
+      ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get a list of 'ERC1155 - Token Transfer Events' by Address
+   * @param {string} address the address to check for balance
+   * @param {string} contractAddress the token contract address to check for balance
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
+   * @param {number} options.page page number, if pagination is enabled
+   * @param {number} options.offset the number of transactions displayed per page
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
+   * @returns Returns the list of ERC-1155 ( Multi Token Standard ) tokens transferred by an address, with optional filtering by token contract.
+   */
+  public async getERC1155TokenEvents(
+    address: string,
+    contractAddress: string,
+    { ...options }: TransactionOptions = {}
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'token1155tx', {
+      address,
+      contractAddress,
+      ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get list of Blocks Validated by Address
+   * @param {string} address the address to check for balance
+   * @param {string} blockType the pre-defined block type, either `blocks` for canonical blocks or `uncles` for uncle blocks only
+   * @param {number} options.page page number, if pagination is enabled
+   * @param {number} options.offset the number of transactions displayed per page
+   * @returns Returns the list of blocks validated by an address.
+   */
+  public async getValidatedBlocks(
+    address: string,
+    blockType: BlockType,
+    { ...options }: BlockValidationOptions = {}
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'getminedblocks', {
+      address,
+      blockType,
+      ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get Beacon Chain Withdrawals by Address and Block Range
+   * @param {string} address the address to check for balance
+   * @param {number} options.startBlock block number to start searching for transactions
+   * @param {number} options.endBlock block number to stop searching for transactions
+   * @param {number} options.page page number, if pagination is enabled
+   * @param {number} options.offset the number of transactions displayed per page
+   * @param {string} options.sort the sorting preference, use `asc` to sort by ascending and `desc` to sort by descendin. Tip: Specify a smaller startBlock and endBlock range for faster search results.
+   * @returns Returns the beacon chain withdrawals made to an address.
+   */
+  public async getBeaconChainWithdrawals(
+    address: string,
+    { ...options }: TransactionOptions = {}
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'txsBeaconWithdrawal', {
+      address,
+      ...options,
+    });
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  }
+
+  /**
+   * Get Historical Ether Balance for a Single Address By BlockNo (PRO Tier Only)
+   * @param {string} address the address to check for balance
+   * @param {number} blockNo block number to check balance for e.g. `12697906`
+   * @returns Returns the beacon chain withdrawals made to an address.
+   */
+  public async getHistoricalBalance(
+    address: string,
+    blockNo: number
+  ): Promise<BalanceResponse> {
+    const url = this.etherScan.constructUrl('account', 'balancehistory', {
+      address,
+      blockNo,
     });
     const response = await fetch(url);
     if (!response.ok) {
